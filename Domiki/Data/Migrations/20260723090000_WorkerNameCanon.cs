e549972e2b5aea1e866b858d6bd4e83e36645bc8
Савелий Вотинцев <votintcev1@mail.ru>
2026-07-23 22:58:09 +0300

    Лорные имена трудяг
    - Заменены выбивавшиеся из деревенского уклада имена: Сава на Савва, Агата на Агафья, Есения на Аксинья, Марта на Марфа
    - Пул имён трудяг расширен с 32 до 56 старорусскими формами
    - Добавлены портреты (workerLooks) новым именам, женские формы получили женский облик
    - Добавлена миграция WorkerNameCanon: переименование уже нанятых трудяг со старыми написаниями

---
+++ Domiki/Data/Migrations/20260723090000_WorkerNameCanon.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domiki.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class WorkerNameCanon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Савва' WHERE \"name\" = 'Сава';");
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Агафья' WHERE \"name\" = 'Агата';");
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Аксинья' WHERE \"name\" = 'Есения';");
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Марфа' WHERE \"name\" = 'Марта';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Сава' WHERE \"name\" = 'Савва';");
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Агата' WHERE \"name\" = 'Агафья';");
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Есения' WHERE \"name\" = 'Аксинья';");
+            migrationBuilder.Sql("UPDATE \"workers\" SET \"name\" = 'Марта' WHERE \"name\" = 'Марфа';");
+        }
+    }
+}
